0acb882d3b4891062731bc85b6118eee0752dee4
greemaltend4 <dengestabiy@hotmail.com>
2025-09-24 14:10:30 +0000

    Fix typo in greeting message

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-    res.send('Bienvenue sur Gestion Cloud Integration!');
+    res.send('Bienvenue sur la Gestion de l\'Intégration Cloud!');
 });
 
 app.listen(PORT, () => {
