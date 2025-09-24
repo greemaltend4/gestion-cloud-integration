e183b44c6c43f2c802d4290c9539567ee83b92c5
greemaltend4 <dengestabiy@hotmail.com>
2025-09-24 14:10:41 +0000

    Fix typo in welcome message for better clarity.

--- index.js
+++ index.js
@@ -3,9 +3,9 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-    res.send('Bienvenue sur la Gestion de l\'Intégration Cloud!');
+    res.send('Bienvenue sur la Gestion de l\'Intégration Cloud !');
 });
 
 app.listen(PORT, () => {
-    console.log(`Serveur en cours d'exécution sur le port ${PORT}`);
+    console.log(`Serveur en cours d\'exécution sur le port ${PORT}`);
 });
