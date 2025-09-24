a2d3170b407f442d2f9a46ca3f969685e859bed8
greemaltend4 <dengestabiy@hotmail.com>
2025-09-24 14:13:30 +0000

    Add a port configuration and server initialization to app.js

--- app.js
+++ app.js
@@ -17,4 +17,9 @@ app.use((req, res, next) => {
 
 // Routes here...
 
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
+
 module.exports = app;
