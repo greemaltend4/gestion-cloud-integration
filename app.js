5b53896ee6e79158c00ccf5070737750befd289b
greemaltend4 <dengestabiy@hotmail.com>
2025-09-24 14:12:11 +0000

    Add helmet middleware for enhanced security

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
+const helmet = require('helmet');
 
 const app = express();
 
+app.use(helmet());
 app.use(cors());
 app.use(bodyParser.json());
 
