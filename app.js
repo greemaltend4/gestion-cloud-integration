0217466905bb97a5c948c8072f0e151f892c0656
greemaltend4 <dengestabiy@hotmail.com>
2025-09-24 14:12:40 +0000

    Add logging middleware for better request tracking

--- app.js
+++ app.js
@@ -9,6 +9,12 @@ app.use(helmet());
 app.use(cors());
 app.use(bodyParser.json());
 
+// Logging middleware
+app.use((req, res, next) => {
+    console.log(`${req.method} ${req.url}`);
+    next();
+});
+
 // Routes here...
 
 module.exports = app;
